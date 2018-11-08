0af97ee29b009b8b381557e13f04c4802b9d4dc0
rradjabi <rradjabi@gmail.com>
2018-11-08 10:35:36 -0800

    Adds components fooApp and barApp and enables component install where helloApp is in the default component. -BAR -FOO and -Unspecified rpms generated.

---
+++ src/bar.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+
+int main( void ) {
+    std::cout << "Hello BAR!\n";
+    return 0;
+}
---
+++ src/foo.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+
+int main( void ) {
+    std::cout << "Hello FOO!\n";
+    return 0;
+}
