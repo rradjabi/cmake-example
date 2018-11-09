96faae168f7ac639f2c7ac253ae4ef00760cfa6f
rradjabi <rradjabi@gmail.com>
2018-11-09 13:12:24 -0800

    modified build.sh

--- build/build.sh
+++ build/build.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
+# test
 
-#if RHEL/CENTOS
-CMAKE_EXE=cmake3
-#else
-#CMAKE_EXE=cmake
-#endif
+FLAVOR=`grep '^ID=' /etc/os-release | awk -F= '{print $2}'`
+FLAVOR=`echo $FLAVOR | tr -d '"'`
+echo "Flavor: $FLAVOR"
+CMAKE_EXE=cmake
+if [ $FLAVOR == "centos" ] || [ $FLAVOR == "rhel" ] ; then
+    CMAKE_EXE=cmake3
+fi
+echo "CMake: $CMAKE_EXE"
 
 here=$PWD
 mkdir -p Release
